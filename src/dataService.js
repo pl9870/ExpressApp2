38177478428ad54dd01f84ba526d6a54ee49ccd0
Manu Gupta <mgupta8143@townsendharris.org>
2020-01-07 16:03:33 -0500

    GCI pl9870

--- src/dataService.js
+++ src/dataService.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let products = require('./data/products.json');
 let categories = require('./data/categories.json');
 
@@ -5,29 +7,31 @@ let productMap = new Map();
 let categoryMap = new Map();
 let combinedProductMap = new Map();
 
-//returns a map of elements with product id with product
+// returns a map of elements with product id with product
 function getProducts() {
-   products.products.forEach(product => productMap.set(product["id"], product));
-   return productMap;
+  products.products.forEach(product => productMap.set(product['id'], product));
 }
 
-//returns a map of elements with category id and category
+// returns a map of elements with category id and category
 function getCategories() {
-   categories.categories.forEach(category => categoryMap.set(category["id"], category));
-   return categoryMap;
+  categories.categories.forEach(cat => categoryMap.set(cat['id'], cat));
 }
 
-//returns a map of elements with product id and category and products combined
+// returns a map of elements with product id and category and products combined
 function combineProductsWithCategories() {
-  products.products.map(product => product.categoryName = categories.categories.filter(category => category.id === product.categoryId)[0].categoryName).forEach(obj => combinedProductMap.set(obj["id"], obj));
+  products.products.map(function(p) {
+    p.categoryName =
+    categories.categories.filter(c => c.id ===
+    p.categoryId)[0].categoryName;
+    combinedProductMap.set(p['id'], p);
+  });
 }
 
 function getCombinedProductMap() {
-  if(combinedProductMap.size === 0) {
-    combineProductsWithCategories;
+  if (combinedProductMap.size === 0) {
+    combineProductsWithCategories();
   }
   return combinedProductMap;
 }
 
-exports.getProducts = getProducts;
-exports.getCategories = getCategories;
+exports.getCombinedProductMap = getCombinedProductMap;
--- src/index.js
+++ src/index.js
@@ -1,38 +1,42 @@
-"use strict";
-const express = require('express');
-const app = express();
+'use strict';
+let express = require('express');
+let app = express();
 let products = require('./data/products.json');
 let categories = require('./data/categories.json');
 let dataService = require('./dataService.js');
 
-const PORT = 3000;
+let PORT = 3000;
 
-//combining product.json with categories.json so that categoryId is the actual name of the category
-products.products.map(product => product.categoryName = categories.categories.filter(category => category.id === product.categoryId)[0].categoryName);
+// combining product.json with categories.json
+products.products.map(function(p) {
+  p.categoryName =
+  categories.categories.filter(c => c.id ===
+  p.categoryId)[0].categoryName;
+});
 
 app.get('/', (request, response) => {
   response.send('Hello World');
 });
 
-//returning all products
+// returning all products
 app.get('/products/all', (request, response) => {
-  response.send(Array.from(dataService.getProducts()));
+  response.send(Array.from(dataService.getCombinedProductMap()));
 });
 
-//returns products with the specified id
+// returns products with the specified id
 app.get('/products/:id', (request, response) => {
-  for (const [key, value] of dataService.getProducts()) {
-    if(key === request.params.id) {
+  for (const [key, value] of dataService.getCombinedProductMap()) {
+    if (key === request.params.id) {
       response.send(JSON.stringify(value));
     }
   }
 });
 
-//returns products with specified category id
+// returns products with specified category id
 app.get('/category/:ctyId', (request, response) => {
   let arr = [];
-  for (const [key, value] of dataService.getProducts()) {
-    if(value.categoryId === request.params.ctyId) {
+  for (const [key, value] of dataService.getCombinedProductMap()) {
+    if (value.categoryId === request.params.ctyId) {
       arr.push(value);
     }
   }
