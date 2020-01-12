c355640edfe2c8020219ecfe4a13b43dd7339324
pl9870 <31375851+pl9870@users.noreply.github.com>
2020-01-12 15:55:56 -0500

    Update dataService.js

--- src/dataService.js
+++ src/dataService.js
@@ -1,12 +1,10 @@
-const products = require('./data/products.json');
-const categories = require('./data/categories.json');
-
 let productMap = new Map();
 let categoryMap = new Map();
 let combinedProductMap = new Map();
 
 // returns a map of elements with product id with product
 function getProducts() {
+  const products = require('./data/products.json');
   if (productMap.size === 0) {
     products.products.forEach(prod => productMap.set(prod['id'], prod));
   }
@@ -14,6 +12,7 @@ function getProducts() {
 
 // returns a map of elements with category id and category
 function getCategories() {
+  const categories = require('./data/categories.json');
   if (categoryMap.size === 0) {
     categories.categories.forEach(cat => categoryMap.set(cat['id'], cat));
   }
@@ -21,6 +20,8 @@ function getCategories() {
 
 // returns a map of elements with product id and category and products combined
 function combineProductsWithCategories() {
+  const products = require('./data/products.json');
+  const categories = require('./data/categories.json');
   products.products.forEach(function(prod) {
     prod.categoryName = categories.categories.filter(cat => cat.id === prod.categoryId)[0].categoryName;
     combinedProductMap.set(prod['id'], prod);
